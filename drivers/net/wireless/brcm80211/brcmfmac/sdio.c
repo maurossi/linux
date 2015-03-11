36377bc808c77df058301d022b5a46abd2b2c169
Arend van Spriel <arend@broadcom.com>
2015-03-11 16:11:27 +0100

    brcmfmac: remove duplication of ramsize info
    Removing the ramsize from the brcmf_sdio structure to avoid
    duplication. The information is available in brcmf_chip
    structure.
    
    Reviewed-by: Hante Meuleman <meuleman@broadcom.com>
    Reviewed-by: Pieter-Paul Giesberts <pieterpg@broadcom.com>
    Signed-off-by: Arend van Spriel <arend@broadcom.com>

--- drivers/net/wireless/brcm80211/brcmfmac/sdio.c
+++ drivers/net/wireless/brcm80211/brcmfmac/sdio.c
@@ -1072,8 +1072,7 @@ static int brcmf_sdio_readshared(struct brcmf_sdio *bus,
 	struct sdpcm_shared_le sh_le;
 	__le32 addr_le;
 
-	sdio_claim_host(bus->sdiodev->func[1]);
-	brcmf_sdio_bus_sleep(bus, false, false);
+	shaddr = bus->ci->rambase + bus->ci->ramsize - 4;
 
 	/*
 	 * Read last word in socram to determine
