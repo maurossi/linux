30d0e3e675da41295e6938a666ef42c5f871ba37
Mauro Rossi <issor.oruam@gmail.com>
2015-06-23 03:55:04 +0200

    fix drivers/mfd/intel_soc_pmic_core.c building error

--- drivers/mfd/intel_soc_pmic_core.c
+++ drivers/mfd/intel_soc_pmic_core.c
@@ -24,8 +24,6 @@
 #include <linux/acpi.h>
 #include <linux/regmap.h>
 #include <linux/mfd/intel_soc_pmic.h>
-#include <linux/gpio/machine.h>
-#include <linux/pwm.h>
 #include "intel_soc_pmic_core.h"
 
 static int intel_soc_pmic_find_gpio_irq(struct device *dev)
@@ -87,12 +85,6 @@ static int intel_soc_pmic_i2c_probe(struct i2c_client *i2c,
 	if (ret)
 		dev_warn(dev, "Can't enable IRQ as wake source: %d\n", ret);
 
-	/* Add lookup table binding for Panel Control to the GPIO Chip */
-	gpiod_add_lookup_table(&panel_gpio_table);
-
-	/* Add lookup table for crc-pwm */
-	pwm_add_table(crc_pwm_lookup, ARRAY_SIZE(crc_pwm_lookup));
-
 	ret = mfd_add_devices(dev, -1, config->cell_dev,
 			      config->n_cell_devs, NULL, 0,
 			      regmap_irq_get_domain(pmic->irq_chip_data));
@@ -111,12 +103,6 @@ static int intel_soc_pmic_i2c_remove(struct i2c_client *i2c)
 	struct intel_soc_pmic *pmic = dev_get_drvdata(&i2c->dev);
 
 	regmap_del_irq_chip(pmic->irq, pmic->irq_chip_data);
-
-	/* Remove lookup table for Panel Control from the GPIO Chip */
-	gpiod_remove_lookup_table(&panel_gpio_table);
-
-	/* remove crc-pwm lookup table */
-	pwm_remove_table(crc_pwm_lookup, ARRAY_SIZE(crc_pwm_lookup));
 
 	mfd_remove_devices(&i2c->dev);
 
