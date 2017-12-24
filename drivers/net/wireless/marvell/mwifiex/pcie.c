ef38c496392868b0bcf372c78beac93645c4b845
Jake Day <jake@ninebysix.com>
2017-12-24 14:04:35 -0500

    cancel pending work on shutting down wifi to prevent hangs

--- drivers/net/wireless/marvell/mwifiex/pcie.c
+++ drivers/net/wireless/marvell/mwifiex/pcie.c
@@ -455,6 +455,8 @@ static void mwifiex_pcie_remove(struct pci_dev *pdev)
 		mwifiex_init_shutdown_fw(priv, MWIFIEX_FUNC_SHUTDOWN);
 	}
 
+	cancel_work_sync(&card->work);
+
 	mwifiex_remove_card(adapter);
 }
 
