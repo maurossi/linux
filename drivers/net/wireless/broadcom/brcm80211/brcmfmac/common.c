f9a12e05508af800e8e1c103cdf9d11d5fed99a0
Bjorn Andersson <bjorn.andersson@linaro.org>
2018-01-15 09:10:53 -0800

    brcmfmac: Make sure CLM downloading is optional
    The presence of a CLM file is described as optional, but missing the clm
    blob causes the preinit to return unsuccessfully. Fix this by ignoring
    the return value of the brcmf_c_process_clm_blob().
    
    Also remove the extra debug print, as brcmf_c_process_clm_blob() already
    did print a useful error message before returning.
    
    Fixes: fdd0bd88ceae ("brcmfmac: add CLM download support")
    Cc: stable@vger.kernel.org
    Signed-off-by: Bjorn Andersson <bjorn.andersson@linaro.org>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/common.c
@@ -257,12 +257,8 @@ int brcmf_c_preinit_dcmds(struct brcmf_if *ifp)
 		brcmf_chip_name(bus->chip, bus->chiprev,
 				ri->chipname, sizeof(ri->chipname));
 
-	/* Do any CLM downloading */
-	err = brcmf_c_process_clm_blob(ifp);
-	if (err < 0) {
-		brcmf_err("download CLM blob file failed, %d\n", err);
-		goto done;
-	}
+	/* Do any optional CLM downloading */
+	brcmf_c_process_clm_blob(ifp);
 
 	/* query for 'ver' to get version info from firmware */
 	memset(buf, 0, sizeof(buf));
