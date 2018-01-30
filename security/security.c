16db509d69e568b364a4ad3260c40e9911e10ce5
Daniel Rosenberg <drosen@google.com>
2018-01-30 14:24:02 -0800

    ANDROID: sdcardfs: Enable modular sdcardfs
    Export the following symbols:
    
    - copy_fs_struct
    - free_fs_struct
    - security_path_chown
    - set_fs_pwd
    - vfs_read
    - vfs_write
    
    These are needed to build sdcardfs as a module.
    
    Test: HiKey/X15 + Pie + android-mainline,
          and HiKey + AOSP Maser + android-mainline,
          directories under /sdcard created,
          output of mount is right,
          CTS test collecting device infor works
    
    Bug: 35142419
    Change-Id: If6e14f0b3bdc858a9f684e6c209927a9232091f0
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    [adelva: Folded the following changes into this patch:
             e19f69662df5 ("ANDROID: Revert "fs: unexport vfs_read and vfs_write"")
             17071a8e1e7d ("ANDROID: fs: Export free_fs_struct and set_fs_pwd")
             2e9a639597cd ("ANDROID: export security_path_chown")]
    Signed-off-by: Alistair Delva <adelva@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- security/security.c
+++ security/security.c
@@ -1971,6 +1971,7 @@ int security_path_chown(const struct path *path, kuid_t uid, kgid_t gid)
 		return 0;
 	return call_int_hook(path_chown, path, uid, gid);
 }
+EXPORT_SYMBOL(security_path_chown);
 
 /**
  * security_path_chroot() - Check if changing the root directory is allowed
