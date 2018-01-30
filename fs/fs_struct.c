769eb6a2f3e614f952cb87031000aca42940194c
Daniel Rosenberg <drosen@google.com>
2018-01-30 14:24:02 -0800

    ANDROID: sdcardfs: Enable modular sdcardfs
    Export the following symbols:
    
    - copy_fs_struct
    - free_fs_struct
    - security_path_chown
    - set_fs_pwd
    - vfs_read
    - vfs_write
    
    These are needed to build sdcardfs as a module.
    
    Test: HiKey/X15 + Pie + android-mainline,
          and HiKey + AOSP Maser + android-mainline,
          directories under /sdcard created,
          output of mount is right,
          CTS test collecting device infor works
    
    Bug: 35142419
    Change-Id: If6e14f0b3bdc858a9f684e6c209927a9232091f0
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    [adelva: Folded the following changes into this patch:
             e19f69662df5 ("ANDROID: Revert "fs: unexport vfs_read and vfs_write"")
             17071a8e1e7d ("ANDROID: fs: Export free_fs_struct and set_fs_pwd")
             2e9a639597cd ("ANDROID: export security_path_chown")]
    Signed-off-by: Alistair Delva <adelva@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>

--- fs/fs_struct.c
+++ fs/fs_struct.c
@@ -42,6 +42,7 @@ void set_fs_pwd(struct fs_struct *fs, const struct path *path)
 	if (old_pwd.dentry)
 		path_put(&old_pwd);
 }
+EXPORT_SYMBOL(set_fs_pwd);
 
 static inline int replace_path(struct path *p, const struct path *old, const struct path *new)
 {
@@ -85,6 +86,7 @@ void free_fs_struct(struct fs_struct *fs)
 	path_put(&fs->pwd);
 	kmem_cache_free(fs_cachep, fs);
 }
+EXPORT_SYMBOL(free_fs_struct);
 
 void exit_fs(struct task_struct *tsk)
 {
@@ -122,6 +124,7 @@ struct fs_struct *copy_fs_struct(struct fs_struct *old)
 	}
 	return fs;
 }
+EXPORT_SYMBOL_GPL(copy_fs_struct);
 
 int unshare_fs_struct(void)
 {
