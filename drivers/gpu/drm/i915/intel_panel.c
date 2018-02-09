c776aaf043fdba0d0f767a8b827c8b4cbdbf8d1f
lambdadroid <lambdadroid@gmail.com>
2018-02-09 12:46:08 +0100

    ANDROID: drm/i915: Avoid resetting brightness if set to minimum (v2)
    Right now, the brightness is reset to maximum if it was set to
    minimum before the backlight was disabled. This looks bad when
    turning the screen back on because it will actually fade down
    instead of fade up when a lower brightness is set.
    
    Just keep the brightness as-is after turning backlight on so
    userspace can directly set the correct brightness value.
    
    v2: Keep check but reset brightness only if below minimum.

--- drivers/gpu/drm/i915/intel_panel.c
+++ drivers/gpu/drm/i915/intel_panel.c
@@ -1104,7 +1104,7 @@ void intel_panel_enable_backlight(const struct intel_crtc_state *crtc_state,
 
 	WARN_ON(panel->backlight.max == 0);
 
-	if (panel->backlight.level <= panel->backlight.min) {
+	if (panel->backlight.level < panel->backlight.min) {
 		panel->backlight.level = panel->backlight.max;
 		if (panel->backlight.device)
 			panel->backlight.device->props.brightness =
