14ebf2287b0e4f3bfd74f1aabb5447ce00077f20
Mauro Rossi <issor.oruam@gmail.com>
2018-02-25 09:44:08 +0100

    staging: rtl8812au: add updated driver
    Cloned from https://github.com/lwfinger/rtl8812au
    branch master commit bc5f254 (stable driver v4.2.2)
    and removed .git/ folder and .gitignore file

---
+++ drivers/staging/rtl8812au/include/rtl8812a_led.h
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ ******************************************************************************/
+#ifndef __RTL8812A_LED_H__
+#define __RTL8812A_LED_H__
+
+
+//================================================================================
+// Interface to manipulate LED objects.
+//================================================================================
+void rtl8812au_InitSwLeds(PADAPTER padapter);
+void rtl8812au_DeInitSwLeds(PADAPTER padapter);
+
+#endif
