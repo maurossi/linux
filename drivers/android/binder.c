9381f0b8ef6a61d001389c9150f4983a94164589
Mauro Rossi <issor.oruam@gmail.com>
2018-06-16 13:39:50 +0200

    Revert "ANDROID: binder: remove 32-bit binder interface."
    This reverts commit 1190b4e38f97023154e6b3bef61b251aa5f970d0.
    
     Conflicts:
    	drivers/android/binder.c

--- drivers/android/binder.c
+++ drivers/android/binder.c
@@ -67,6 +67,10 @@
 #include <linux/task_work.h>
 #include <linux/sizes.h>
 
+#ifdef CONFIG_ANDROID_BINDER_IPC_32BIT
+#define BINDER_IPC_32BIT 1
+#endif
+
 #include <uapi/linux/sched/types.h>
 #include <uapi/linux/android/binder.h>
 
