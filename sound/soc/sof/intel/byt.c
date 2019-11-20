f78abac5d86e4457533c3483e4d82b6f4069888c
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2019-11-20 10:59:41 -0800

    ASoC: SOF: Intel: byt: Add PM callbacks
    Add the PM callbacks for BYT/CHT platforms.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>

--- sound/soc/sof/intel/byt.c
+++ sound/soc/sof/intel/byt.c
@@ -455,7 +455,7 @@ static void byt_set_mach_params(const struct snd_soc_acpi_mach *mach,
 	mach_params->platform = dev_name(dev);
 }
 
-static int byt_remove(struct snd_sof_dev *sdev)
+static int byt_reset_dsp_disable_int(struct snd_sof_dev *sdev)
 {
 	/* disable Interrupt from both sides */
 	snd_sof_dsp_update_bits64(sdev, BYT_DSP_BAR, SHIM_IMRX, 0x3, 0x3);
@@ -465,6 +465,25 @@ static int byt_remove(struct snd_sof_dev *sdev)
 	snd_sof_dsp_update_bits64(sdev, BYT_DSP_BAR, SHIM_CSR,
 				  SHIM_BYT_CSR_RST | SHIM_BYT_CSR_VECTOR_SEL,
 				  SHIM_BYT_CSR_RST | SHIM_BYT_CSR_VECTOR_SEL);
+
+	return 0;
+}
+
+static int byt_remove(struct snd_sof_dev *sdev)
+{
+	return byt_reset_dsp_disable_int(sdev);
+}
+
+static int byt_suspend(struct snd_sof_dev *sdev)
+{
+	return byt_reset_dsp_disable_int(sdev);
+}
+
+static int byt_resume(struct snd_sof_dev *sdev)
+{
+	/* enable Interrupt from both sides */
+	snd_sof_dsp_update_bits64(sdev, BYT_DSP_BAR, SHIM_IMRX, 0x3, 0x0);
+	snd_sof_dsp_update_bits64(sdev, BYT_DSP_BAR, SHIM_IMRD, 0x3, 0x0);
 
 	return 0;
 }
@@ -799,6 +818,12 @@ const struct snd_sof_dsp_ops sof_byt_ops = {
 	/*Firmware loading */
 	.load_firmware	= snd_sof_load_firmware_memcpy,
 
+	/* PM */
+	.suspend = byt_suspend,
+	.resume = byt_resume,
+	.runtime_suspend = byt_suspend,
+	.runtime_resume = byt_resume,
+
 	/* DAI drivers */
 	.drv = byt_dai,
 	.num_drv = 3, /* we have only 3 SSPs on byt*/
@@ -873,6 +898,12 @@ const struct snd_sof_dsp_ops sof_cht_ops = {
 
 	/*Firmware loading */
 	.load_firmware	= snd_sof_load_firmware_memcpy,
+
+	/* PM */
+	.suspend = byt_suspend,
+	.resume = byt_resume,
+	.runtime_suspend = byt_suspend,
+	.runtime_resume = byt_resume,
 
 	/* DAI drivers */
 	.drv = byt_dai,
