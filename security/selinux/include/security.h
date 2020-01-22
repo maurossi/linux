7116cedf9f77fa7a8e410d8db054135885bd6dff
Jeff Vander Stoep <jeffv@google.com>
2020-01-22 11:19:58 +0100

    ANDROID: selinux: modify RTM_GETLINK permission
    Map the permission gating RTM_GETLINK messages to a new permission so
    that it can be distinguished from the other netlink route permissions
    in selinux policy.
    
    This is a temporary Android-only patch that will be deprecated in
    newer kernels once the long-term solution lands as discusssed on the
    mailing list [1]. The maintainer's recommended solution is more
    general, much more complex, and likely not suitable for backporting.
    This patch provides the minimal change needed for Android including
    the userspace settable trigger which ensures that the permission
    change is only applied to the newest version of Android which
    contains the changes needed for userpace compatibility.
    
    [1]: https://lore.kernel.org/selinux/20200116142653.61738-1-jeffv@google.com/
    
    [CPNOTE: 02/07/21] Lee: An upstream solution was in progress - poked the bug for an update
    
    Bug: 141455849
    Bug: 148218425
    
    Test: CtsSelinuxTargetSdkCurrentTestCases
    Test: atest bionic-unit-tests-static
    Test: atest NetworkInterfaceTest
    Test: Connect to Wi-Fi network
    Test: Set up hotspot
    Test: Cast from device
    Test: Pair Bluetooth device
    Test: Call getifaddrs() directly from within an app.
    Test: Call NetworkInterface#getNetworkInterfaces() from within an app.
    
    Change-Id: I7b44ce60ad98f858c412722d41b9842f8577151f
    Signed-off-by: Jeff Vander Stoep <jeffv@google.com>

--- security/selinux/include/security.h
+++ security/selinux/include/security.h
@@ -97,6 +97,7 @@ struct selinux_state {
 	bool checkreqprot;
 	bool initialized;
 	bool policycap[__POLICYDB_CAPABILITY_MAX];
+	bool android_netlink_route;
 
 	struct page *status_page;
 	struct mutex status_lock;
@@ -217,6 +218,13 @@ static inline bool selinux_policycap_genfs_seclabel_symlinks(void)
 	struct selinux_state *state = &selinux_state;
 
 	return READ_ONCE(state->policycap[POLICYDB_CAPABILITY_GENFS_SECLABEL_SYMLINKS]);
+}
+
+static inline bool selinux_android_nlroute_getlink(void)
+{
+	struct selinux_state *state = &selinux_state;
+
+	return state->android_netlink_route;
 }
 
 struct selinux_policy_convert_data;
@@ -452,5 +460,6 @@ extern void avtab_cache_init(void);
 extern void ebitmap_cache_init(void);
 extern void hashtab_cache_init(void);
 extern int security_sidtab_hash_stats(struct selinux_state *state, char *page);
+extern void selinux_nlmsg_init(void);
 
 #endif /* _SELINUX_SECURITY_H_ */
