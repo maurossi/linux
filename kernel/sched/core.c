2afe449871dc88579f050189b54092dc452c32c3
Todd Kjos <tkjos@google.com>
2020-03-27 10:04:16 -0700

    Revert "sched/core: Prevent race condition between cpuset and __sched_setscheduler()"
    This reverts commit 710da3c8ea7dfbd327920afd3831d8c82c42789d.
    
    When changing a thread's scheduling priority, binder calls
    sched_setscheduler_nocheck() while holding the node lock and
    proc inner lock. This was safe until v5.3 when this change was
    introduced where cpuset_read_lock() is called in this path
    which can sleep.
    
    This patch was introduced to fix a possible accounting error
    in sched deadline (potential oversell of CPU bandwidth) due
    to a race condition between cpusets and __sched_setscheduler().
    This is not an issue for Android.
    
    This should be fixed in the binder driver, but that may take
    some time.
    
    [CPNOTE: 21/07/21] Lee: Requested status from the author via the bug
    [CPNOTE: 07/09/21] Lee: Required whilst carrying current Binder PI implementation
    
    Bug: 120446518 # OoT
    Bug: 151861772
    Change-Id: Ica1ef71b3cdcdc509b341ea1b57c41f8ee73794a
    Signed-off-by: Todd Kjos <tkjos@google.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7348,9 +7348,6 @@ recheck:
 			return retval;
 	}
 
-	if (pi)
-		cpuset_read_lock();
-
 	/*
 	 * Make sure no PI-waiters arrive (or leave) while we are
 	 * changing the priority of the task:
@@ -7425,8 +7422,6 @@ change:
 	if (unlikely(oldpolicy != -1 && oldpolicy != p->policy)) {
 		policy = oldpolicy = -1;
 		task_rq_unlock(rq, p, &rf);
-		if (pi)
-			cpuset_read_unlock();
 		goto recheck;
 	}
 
@@ -7492,10 +7487,8 @@ change:
 	head = splice_balance_callbacks(rq);
 	task_rq_unlock(rq, p, &rf);
 
-	if (pi) {
-		cpuset_read_unlock();
+	if (pi)
 		rt_mutex_adjust_pi(p);
-	}
 
 	/* Run balance callbacks after we've adjusted the PI chain: */
 	balance_callbacks(rq, head);
@@ -7505,8 +7498,6 @@ change:
 
 unlock:
 	task_rq_unlock(rq, p, &rf);
-	if (pi)
-		cpuset_read_unlock();
 	return retval;
 }
 
@@ -7636,14 +7627,9 @@ do_sched_setscheduler(pid_t pid, int policy, struct sched_param __user *param)
 	rcu_read_lock();
 	retval = -ESRCH;
 	p = find_process_by_pid(pid);
-	if (likely(p))
-		get_task_struct(p);
-	rcu_read_unlock();
-
-	if (likely(p)) {
+	if (p != NULL)
 		retval = sched_setscheduler(p, policy, &lparam);
-		put_task_struct(p);
-	}
+	rcu_read_unlock();
 
 	return retval;
 }
