f8ee3bc4c75ba5d1b3ff9ea7b4938ce47207af68
JianMin Liu <jian-min.liu@mediatek.com>
2020-08-11 15:24:06 +0800

    NOUPSTREAM: ANDROID: futex: Add vendor hook for wait queue
    Add the hook for the waiter list of futex to allow
    vendor perform wait queue enhancement
    
    [CPNOTE: 27/05/21] Lee: Vendor related code - maintain forever
    
    Bug: 163431711
    
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Change-Id: I68218b89c35b23aa5529099bb0bbbd031bdeafef

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -10,6 +10,7 @@
 #include <trace/hooks/vendor_hooks.h>
 #include <trace/hooks/fpsimd.h>
 #include <trace/hooks/rwsem.h>
+#include <trace/hooks/futex.h>
 #include <trace/hooks/mpam.h>
 #include <trace/hooks/debug.h>
 
@@ -22,5 +23,6 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_init);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_wake);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_finished);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_rwsem_list_add);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_alter_futex_plist_add);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mpam_set);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipi_stop);
