aa57d49ca8ca927533bc7907c0dc711a948cd70a
C-J.Chen <C-J.Chen@mediatek.com>
2020-08-18 08:38:07 +0800

    NOUPSTREAM: ANDROID: mpam: add vendor hook to record MPAM
    - To use MPAM in kernel task, vendor_hook call is needed to
      set MPAM at scheduling time.
    - Vendor_hooks is call ed when task is switching for MPAM state
    
    [CPNOTE: 27/05/21] Lee: Vendor related code - maintain forever
    
    Bug: 165333282
    Signed-off-by: C-J.Chen <C-J.Chen@mediatek.com>
    Change-Id: I30270c667c26e1a7039580b33011665bb3a8cce7

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -41,6 +41,7 @@
 #include <linux/thread_info.h>
 #include <linux/prctl.h>
 #include <trace/hooks/fpsimd.h>
+#include <trace/hooks/mpam.h>
 
 #include <asm/alternative.h>
 #include <asm/compat.h>
@@ -504,6 +505,12 @@ __notrace_funcgraph struct task_struct *__switch_to(struct task_struct *prev,
 	ssbs_thread_switch(next);
 	erratum_1418040_thread_switch(prev, next);
 	ptrauth_thread_switch_user(next);
+
+	/*
+	 *  vendor hook is needed before the dsb(),
+	 *  because MPAM is related to cache maintenance.
+	 */
+	trace_android_vh_mpam_set(prev, next);
 
 	/*
 	 * Complete any pending TLB or cache maintenance on this CPU in case
