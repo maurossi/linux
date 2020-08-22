d345bfb742f9f58915340794d983763216865d11
Eric Biggers <ebiggers@google.com>
2020-08-22 10:47:34 -0700

    ONHOLD: ANDROID: scsi: ufs: add UFSHCD_QUIRK_KEYS_IN_PRDT
    Add UFSHCD_QUIRK_KEYS_IN_PRDT which tells ufshcd-core to zeroize the
    PRDT after each encrypted request.
    
    This is needed because when using FMP, encryption keys get stored in the
    PRDT.  Keys should always be zeroized when no longer needed.
    
    [CPNOTE: 19/07/21] Lee: Requested status from Eric via the bug
    [CPNOTE: 21/07/21] Lee: Upstream when "hardware key manager" user appears upstream
    
    Bug: 166139333
    Bug: 162257402
    Change-Id: I4855e276f16742aeaf962a2d344a11db6ff2a544
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- drivers/scsi/ufs/ufshcd.c
+++ drivers/scsi/ufs/ufshcd.c
@@ -5284,6 +5284,7 @@ static void __ufshcd_transfer_req_compl(struct ufs_hba *hba,
 				ufshcd_transfer_rsp_status(hba, lrbp);
 			scsi_dma_unmap(cmd);
 			cmd->result = result;
+			ufshcd_crypto_clear_prdt(hba, lrbp);
 			/* Mark completed command as NULL in LRB */
 			lrbp->cmd = NULL;
 			/* Do not touch lrbp after scsi done */
