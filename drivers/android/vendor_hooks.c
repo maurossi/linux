a0bfb2a9cdb1d6670589590ceaa5034789e2f2b1
Sangmoon Kim <sangmoon.kim@samsung.com>
2020-10-08 10:42:06 +0900

    NOUPSTREAM: ANDROID: sysrq: add vendor hook for sysrq crash information
     - Add the hook to provide additional information
       of sysrq crash.
    
    [CPNOTE: 03/06/21] Lee: Vendor related code - maintain forever
    
    Bug: 170234110
    
    Signed-off-by: Sangmoon Kim <sangmoon.kim@samsung.com>
    Change-Id: Ib8f14eaa639b2ef959e911d611515868ff9f193d
    (cherry picked from commit 32b242337266a3b8e170b0ddc4bec478d5cf1f94)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -20,6 +20,7 @@
 #include <trace/hooks/gic.h>
 #include <trace/hooks/wqlockup.h>
 #include <trace/hooks/debug.h>
+#include <trace/hooks/sysrqcrash.h>
 #include <trace/hooks/printk.h>
 #include <trace/hooks/epoch.h>
 #include <trace/hooks/cpufreq.h>
@@ -70,6 +71,7 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_find_busiest_group);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gic_resume);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_wq_lockup_pool);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ipi_stop);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sysrq_crash);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_dump_throttled_rt_tasks);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_printk_hotplug);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_jiffies_update);
