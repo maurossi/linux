8bf2a7903f8c4ac9fc4d867742c009df5916602b
Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>
2020-10-28 00:02:04 -0700

    NOUPSTREAM: ANDROID: sched: gki: add padding to some structs to support WALT
    Add padding to below structs to support WALT based accounting:
    	1. struct cpu_topology
    	2. struct task_struct
    	3. struct sched_domain_shared
    	4. struct task_group
    	5. struct root_domain
    	6. struct rq
    
    To accommodate potential future changes, reserving more memory than
    what WALT needs today.
    
    [CPNOTE: 30/06/21] Lee: Vendor GKI related changes
    
    Bug: 171858786
    Change-Id: If6d901174fc7963be3ae44daa799cb2953669ec1
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1477,6 +1477,8 @@ struct task_struct {
 	struct callback_head		mce_kill_me;
 	int				mce_count;
 #endif
+	ANDROID_VENDOR_DATA_ARRAY(1, 64);
+	ANDROID_OEM_DATA_ARRAY(1, 6);
 
 #ifdef CONFIG_KRETPROBES
 	struct llist_head               kretprobe_instances;
