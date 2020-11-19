475004d83e4aa289bdc91dd120ae1b005704196e
Will McVicker <willmcvicker@google.com>
2020-11-19 13:46:37 -0800

    NOUPSTREAM: ANDROID: modules: introduce the MODULE_SCMVERSION config
    Config MODULE_SCMVERSION introduces a new module attribute --
    `scmversion` -- which can be used to identify a given module's SCM
    version.  This is very useful for developers that update their kernel
    independently from their kernel modules or vice-versa since the SCM
    version provided by UTS_RELEASE (`uname -r`) will now differ from the
    module's vermagic attribute.
    
    For example, we have a CI setup that tests new kernel changes on the
    hikey960 and db845c devices without updating their kernel modules. When
    these tests fail, we need to be able to identify the exact device
    configuration the test was using. By including MODULE_SCMVERSION, we can
    identify the exact kernel and modules' SCM versions for debugging the
    failures.
    
    Additionally, by exposing the SCM version via the sysfs node
    /sys/module/MODULENAME/scmversion, one can also verify the SCM versions
    of the modules loaded from the initramfs. Currently, modinfo can only
    retrieve module attributes from the module's ko on disk and not from the
    actual module that is loaded in RAM.
    
    You can retrieve the SCM version in two ways,
    
    1) By using modinfo:
        > modinfo -F scmversion MODULENAME
    2) By module sysfs node:
        > cat /sys/module/MODULENAME/scmversion
    
    [CPNOTE: 03/06/21] Lee: NACKed on the upstream MLs
    
    Bug: 180027765
    Link: https://lkml.org/lkml/2021/1/21/1388
    Signed-off-by: Will McVicker <willmcvicker@google.com>
    Change-Id: Ib7c72c72f95c4545adb7cd4e842729557039ce3a

--- kernel/module.c
+++ kernel/module.c
@@ -743,6 +743,7 @@ static struct module_attribute modinfo_##field = {                    \
 
 MODINFO_ATTR(version);
 MODINFO_ATTR(srcversion);
+MODINFO_ATTR(scmversion);
 
 static char last_unloaded_module[MODULE_NAME_LEN+1];
 
@@ -1205,6 +1206,7 @@ static struct module_attribute *modinfo_attrs[] = {
 	&module_uevent,
 	&modinfo_version,
 	&modinfo_srcversion,
+	&modinfo_scmversion,
 	&modinfo_initstate,
 	&modinfo_coresize,
 	&modinfo_initsize,
