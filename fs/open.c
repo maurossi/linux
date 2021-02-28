26b006d8ec12f8b2d0c04c1a78f9a392410116e4
Mauro Rossi <issor.oruam@gmail.com>
2021-02-28 17:22:45 +0100

    ANDROID: vfs: fs: add file and path permissions2 helpers
    Apply permission2 logic for filesystems with per mount permissions
    to file and path permissions, due to commit
    02f92b3868a1 ("fs: add file and path permissions helpers")

--- fs/open.c
+++ fs/open.c
@@ -501,7 +501,7 @@ retry:
 	if (error)
 		goto out;
 
-	error = inode_permission2(path.mnt, path.dentry->d_inode, MAY_EXEC | MAY_CHDIR);
+	error = path_permission2(path.mnt, &path, MAY_EXEC | MAY_CHDIR);
 	if (error)
 		goto dput_and_out;
 
@@ -530,7 +530,7 @@ SYSCALL_DEFINE1(fchdir, unsigned int, fd)
 	if (!d_can_lookup(f.file->f_path.dentry))
 		goto out_putf;
 
-	error = inode_permission2(f.file->f_path.mnt, file_inode(f.file),
+	error = file_permission2(f.file->f_path.mnt, f.file,
 				MAY_EXEC | MAY_CHDIR);
 	if (!error)
 		set_fs_pwd(current->fs, &f.file->f_path);
@@ -550,7 +550,7 @@ retry:
 	if (error)
 		goto out;
 
-	error = inode_permission2(path.mnt, path.dentry->d_inode, MAY_EXEC | MAY_CHDIR);
+	error = path_permission2(path.mnt, &path, MAY_EXEC | MAY_CHDIR);
 	if (error)
 		goto dput_and_out;
 
