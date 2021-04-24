748ed04144ca08a18bec0cd44451682812c7e22e
Mauro Rossi <issor.oruam@gmail.com>
2021-04-24 23:57:39 +0200

    ANDROID: vfs: Add setattr2 for filesystems with per mount permissions (v2)
    This allows filesystems to use their mount private data to
    influence the permssions they use in setattr2. It has
    been separated into a new call to avoid disrupting current
    setattr users.
    
    Test: HiKey/X15 + Pie + android-mainline,
          and HiKey + AOSP Maser + android-mainline,
          directories under /sdcard created,
          output of mount is right,
          CTS test collecting device infor works
    
    Change-Id: I19959038309284448f1b7f232d579674ef546385
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>
    
    (v2)
    Changes due to following commits:
    
    549c729 "fs: make helpers idmap mount aware"
    643fe55 "open: handle idmapped mounts in do_truncate()"
    6521f89 "namei: prepare for idmapped mounts"
    2f221d6 "attr: handle idmapped mounts"
    21cb47b "inode: make init and permission helpers idmapped mount aware"
    47291ba "namei: make permission helpers idmapped mount aware"

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -2132,6 +2132,7 @@ struct inode_operations {
 			struct inode *, struct dentry *, unsigned int);
 	int (*setattr) (struct user_namespace *, struct dentry *,
 			struct iattr *);
+	int (*setattr2) (struct vfsmount *, struct user_namespace *, struct dentry *, struct iattr *);
 	int (*getattr) (struct user_namespace *, const struct path *,
 			struct kstat *, u32, unsigned int);
 	ssize_t (*listxattr) (struct dentry *, char *, size_t);
@@ -2727,6 +2728,8 @@ static inline struct user_namespace *file_mnt_user_ns(struct file *file)
 extern long vfs_truncate(const struct path *, loff_t);
 int do_truncate(struct user_namespace *, struct dentry *, loff_t start,
 		unsigned int time_attrs, struct file *filp);
+extern int do_truncate2(struct vfsmount *, struct user_namespace *, struct dentry *, loff_t start,
+			unsigned int time_attrs, struct file *filp);
 extern int vfs_fallocate(struct file *file, int mode, loff_t offset,
 			loff_t len);
 extern long do_sys_open(int dfd, const char __user *filename, int flags,
@@ -2976,6 +2979,8 @@ static inline int bmap(struct inode *inode,  sector_t *block)
 
 int notify_change(struct user_namespace *, struct dentry *,
 		  struct iattr *, struct inode **);
+extern int notify_change2(struct vfsmount *, struct user_namespace *, struct dentry *,
+			struct iattr *, struct inode **);
 int inode_permission(struct user_namespace *, struct inode *, int);
 int generic_permission(struct user_namespace *, struct inode *, int);
 static inline int file_permission(struct file *file, int mask)
