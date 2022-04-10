7017e234b63b8eabe149c26cdfc0cfd8e107ad98
Mauro Rossi <issor.oruam@gmail.com>
2022-04-10 17:31:56 +0200

    Revert "ANDROID: sdcardfs: Enable modular sdcardfs"
    This reverts commit 69f091044c3ead15facf46ecf67aff3e3cc70751.

--- security/security.c
+++ security/security.c
@@ -1842,7 +1842,6 @@ int security_path_chown(const struct path *path, kuid_t uid, kgid_t gid)
 		return 0;
 	return call_int_hook(path_chown, 0, path, uid, gid);
 }
-EXPORT_SYMBOL(security_path_chown);
 
 /**
  * security_path_chroot() - Check if changing the root directory is allowed
