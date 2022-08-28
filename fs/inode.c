8557f3a7c3e9cef8c62eb8cade60e44be5588904
Mauro Rossi <issor.oruam@gmail.com>
2022-08-29 01:11:46 +0200

    ANDROID: vfs: Add setattr2 for filesystems with per mount permissions (v4)
    This allows filesystems to use their mount private data to
    influence the permssions they use in setattr2. It has
    been separated into a new call to avoid disrupting current
    setattr users.
    
    Test: HiKey/X15 + Pie + android-mainline,
          and HiKey + AOSP Maser + android-mainline,
          directories under /sdcard created,
          output of mount is right,
          CTS test collecting device infor works
    
    Change-Id: I19959038309284448f1b7f232d579674ef546385
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>
    
    (v2)
    Changes due to following commits:
    
    549c729 "fs: make helpers idmap mount aware"
    643fe55 "open: handle idmapped mounts in do_truncate()"
    6521f89 "namei: prepare for idmapped mounts"
    2f221d6 "attr: handle idmapped mounts"
    21cb47b "inode: make init and permission helpers idmapped mount aware"
    47291ba "namei: make permission helpers idmapped mount aware"
    
    (v3)
    Changes due to following commit:
    
    faf99b5 "fs: add __remove_file_privs() with flags parameter"
    
    (v4)
    Changes due to following commit:
    
    41191cf "fs: __file_remove_privs(): restore call to inode_has_no_xattr()"

--- fs/inode.c
+++ fs/inode.c
@@ -2001,7 +2001,7 @@ int dentry_needs_remove_privs(struct dentry *dentry)
 	return mask;
 }
 
-static int __remove_privs(struct user_namespace *mnt_userns,
+static int __remove_privs(struct vfsmount *mnt, struct user_namespace *mnt_userns,
 			  struct dentry *dentry, int kill)
 {
 	struct iattr newattrs;
@@ -2011,7 +2011,7 @@ static int __remove_privs(struct user_namespace *mnt_userns,
 	 * Note we call this on write, so notify_change will not
 	 * encounter any conflicting delegations:
 	 */
-	return notify_change(mnt_userns, dentry, &newattrs, NULL);
+	return notify_change2(mnt, mnt_userns, dentry, &newattrs, NULL);
 }
 
 static int __file_remove_privs(struct file *file, unsigned int flags)
@@ -2032,7 +2032,7 @@ static int __file_remove_privs(struct file *file, unsigned int flags)
 		if (flags & IOCB_NOWAIT)
 			return -EAGAIN;
 
-		error = __remove_privs(file_mnt_user_ns(file), dentry, kill);
+		error = __remove_privs(file->f_path.mnt, file_mnt_user_ns(file), dentry, kill);
 	}
 
 	if (!error)
