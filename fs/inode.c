612737006e661bce6839787f4a43146af2533b21
Mauro Rossi <issor.oruam@gmail.com>
2023-02-28 23:22:58 +0100

    ANDROID: vfs: Add setattr2 for filesystems with per mount permissions (v6)
    This allows filesystems to use their mount private data to
    influence the permssions they use in setattr2. It has
    been separated into a new call to avoid disrupting current
    setattr users.
    
    Test: HiKey/X15 + Pie + android-mainline,
          and HiKey + AOSP Maser + android-mainline,
          directories under /sdcard created,
          output of mount is right,
          CTS test collecting device infor works
    
    Change-Id: I19959038309284448f1b7f232d579674ef546385
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>
    
    (v2)
    Changes due to following commits:
    
    549c729 "fs: make helpers idmap mount aware"
    643fe55 "open: handle idmapped mounts in do_truncate()"
    6521f89 "namei: prepare for idmapped mounts"
    2f221d6 "attr: handle idmapped mounts"
    21cb47b "inode: make init and permission helpers idmapped mount aware"
    47291ba "namei: make permission helpers idmapped mount aware"
    
    (v3)
    Changes due to following commit:
    
    faf99b5 "fs: add __remove_file_privs() with flags parameter"
    
    (v4)
    Changes due to following commit:
    
    41191cf "fs: __file_remove_privs(): restore call to inode_has_no_xattr()"
    
    (v5)
    Changes due to following commit:
    
    05e6295 "Merge tag 'fs.idmapped.v6.3' of git://git.kernel.org/pub/scm/linux/kernel/git/vfs/idmapping"
    
    (v6)
    Changes due to following commit:
    
    d2c84bd "Merge tag 'for-6.9/io_uring-20240310' of git://git.kernel.dk/linux"

--- fs/inode.c
+++ fs/inode.c
@@ -2156,7 +2156,7 @@ int dentry_needs_remove_privs(struct mnt_idmap *idmap,
 	return mask;
 }
 
-static int __remove_privs(struct mnt_idmap *idmap,
+static int __remove_privs(struct vfsmount *mnt, struct mnt_idmap *idmap,
 			  struct dentry *dentry, int kill)
 {
 	struct iattr newattrs;
@@ -2166,7 +2166,7 @@ static int __remove_privs(struct mnt_idmap *idmap,
 	 * Note we call this on write, so notify_change will not
 	 * encounter any conflicting delegations:
 	 */
-	return notify_change(idmap, dentry, &newattrs, NULL);
+	return notify_change2(mnt, idmap, dentry, &newattrs, NULL);
 }
 
 int file_remove_privs_flags(struct file *file, unsigned int flags)
@@ -2187,7 +2187,7 @@ int file_remove_privs_flags(struct file *file, unsigned int flags)
 		if (flags & IOCB_NOWAIT)
 			return -EAGAIN;
 
-		error = __remove_privs(file_mnt_idmap(file), dentry, kill);
+		error = __remove_privs(file->f_path.mnt, file_mnt_idmap(file), dentry, kill);
 	}
 
 	if (!error)
