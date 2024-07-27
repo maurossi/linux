7347ec02419fd2e4685d250228f2cabd3458e297
Mauro Rossi <issor.oruam@gmail.com>
2024-07-27 14:42:55 +0200

    ANDROID: vfs: Add permission2 for filesystems with per mount permissions (v10)
    This allows filesystems to use their mount private data to
    influence the permssions they return in permission2. It has
    been separated into a new call to avoid disrupting current
    permission users.
    
    Test: HiKey/X15 + Pie + android-mainline,
          and HiKey + AOSP Maser + android-mainline,
          directories under /sdcard created,
          output of mount is right,
          CTS test collecting device infor works
    
    Bug: 35848445
    Change-Id: I9d416e3b8b6eca84ef3e336bd2af89ddd51df6ca
    Signed-off-by: Daniel Rosenberg <drosen@google.com>
    [AmitP: Minor refactoring of original patch to align with
            changes from the following upstream commit
            4bfd054 ("fs: fold __inode_permission() into inode_permission()").
            Also introduce vfs_mkobj2(), because do_create()
            moved from using vfs_create() to vfs_mkobj()
            eecec19 ("mqueue: switch to vfs_mkobj(), quit abusing ->d_fsdata")
            do_create() is dropped/cleaned-up upstream so a
            minor refactoring there as well.
            066cc81 ("do_mq_open(): move all work prior to dentry_open() into a helper")]
    Signed-off-by: Amit Pundir <amit.pundir@linaro.org>
    [adelva: Folded the following changes into this patch:
             f46c9d62dd81 ("ANDROID: fs: Export vfs_rmdir2")
             9992eb8b9a1e ("ANDROID: xattr: Pass EOPNOTSUPP to permission2")]
    Signed-off-by: Alistair Delva <adelva@google.com>
    Signed-off-by: Yongqin Liu <yongqin.liu@linaro.org>
    Signed-off-by: Matthias Maennich <maennich@google.com>
    
    (v2)
    Changes due to following commits:
    549c729 "fs: make helpers idmap mount aware"
    643fe55 "open: handle idmapped mounts in do_truncate()"
    6521f89 "namei: prepare for idmapped mounts"
    2f221d6 "attr: handle idmapped mounts"
    21cb47b "inode: make init and permission helpers idmapped mount aware"
    47291ba "namei: make permission helpers idmapped mount aware"
    
    (v3)
    Changes due to following commits:
    34a456e "Merge tag 'fs.idmapped.helpers.v5.13' of git://git.kernel.org/pub/scm/linux/kernel/git/brauner/linux"
    cc15422 "Merge tag 'fs.idmapped.docs.v5.13' of git://git.kernel.org/pub/scm/linux/kernel/git/brauner/linux"
    
    (v4)
    Changes due to following commits:
    c2fd68b "namei: add mapping aware lookup helper"
    0ee50b4 "namei: change filename_parentat() calling conventions"
    
    (v5)
    Changes due to following commit:
    0067501 "fs: add two trivial lookup helpers"
    
    (v6)
    Changes due to following commit:
    6a518af "Merge tag 'fs.acl.rework.v6.2' of git://git.kernel.org/pub/scm/linux/kernel/git/vfs/idmapping"
    
    (v7)
    Changes due to following commit:
    05e6295 "Merge tag 'fs.idmapped.v6.3' of git://git.kernel.org/pub/scm/linux/kernel/git/vfs/idmapping"
    
    (v8)
    Changes due to following commit:
    cdb67fd "rcu pathwalk: prevent bogus hard errors from may_lookup()"
    
    (v9)
    Changes due to following commit:
    5e04975 "Merge branch 'link_path_walk'"
    
    (v10)
    Changes due to following commit:
    f8ffbc3 "Merge tag 'pull-stable-struct_fd' of git://git.kernel.org/pub/scm/linux/kernel/git/viro/vfs"

--- fs/open.c
+++ fs/open.c
@@ -93,7 +93,7 @@ long vfs_truncate(const struct path *path, loff_t length)
 		goto out;
 
 	idmap = mnt_idmap(path->mnt);
-	error = inode_permission(idmap, inode, MAY_WRITE);
+	error = inode_permission2(mnt, idmap, inode, MAY_WRITE);
 	if (error)
 		goto mnt_drop_write_and_out;
 
@@ -477,6 +477,7 @@ static long do_faccessat(int dfd, const char __user *filename, int mode, int fla
 {
 	struct path path;
 	struct inode *inode;
+	struct vfsmount *mnt;
 	int res;
 	unsigned int lookup_flags = LOOKUP_FOLLOW;
 	const struct cred *old_cred = NULL;
@@ -504,6 +505,7 @@ retry:
 		goto out;
 
 	inode = d_backing_inode(path.dentry);
+	mnt = path.mnt;
 
 	if ((mode & MAY_EXEC) && S_ISREG(inode->i_mode)) {
 		/*
@@ -515,7 +517,7 @@ retry:
 			goto out_path_release;
 	}
 
-	res = inode_permission(mnt_idmap(path.mnt), inode, mode | MAY_ACCESS);
+	res = inode_permission2(mnt, mnt_idmap(path.mnt), inode, mode | MAY_ACCESS);
 	/* SuS v2 requires we report a read only fs too */
 	if (res || !(mode & S_IWOTH) || special_file(inode->i_mode))
 		goto out_path_release;
@@ -571,7 +573,7 @@ retry:
 	if (error)
 		goto out;
 
-	error = path_permission(&path, MAY_EXEC | MAY_CHDIR);
+	error = inode_permission2(path.mnt, path.dentry->d_inode, MAY_EXEC | MAY_CHDIR);
 	if (error)
 		goto dput_and_out;
 
@@ -600,7 +602,8 @@ SYSCALL_DEFINE1(fchdir, unsigned int, fd)
 	if (!d_can_lookup(fd_file(f)->f_path.dentry))
 		goto out_putf;
 
-	error = file_permission(fd_file(f), MAY_EXEC | MAY_CHDIR);
+	error = inode_permission2(fd_file(f)->f_path.mnt, file_inode(fd_file(f)),
+				MAY_EXEC | MAY_CHDIR);
 	if (!error)
 		set_fs_pwd(current->fs, &fd_file(f)->f_path);
 out_putf:
@@ -619,7 +622,7 @@ retry:
 	if (error)
 		goto out;
 
-	error = path_permission(&path, MAY_EXEC | MAY_CHDIR);
+	error = inode_permission2(path.mnt, path.dentry->d_inode, MAY_EXEC | MAY_CHDIR);
 	if (error)
 		goto dput_and_out;
 
