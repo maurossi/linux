55e310f24d95e69de31fbe5e6b4454e1e90a4601
Mauro Rossi <issor.oruam@gmail.com>
2025-10-10 00:03:52 +0200

    ANDROID: mnt: Add filesystem private data to mount points (v6)
    (v6)
    Changes due to commit:
    5d132cf "setup_mnt(): primitive for connecting a mount to filesystem"

--- fs/namespace.c
+++ fs/namespace.c
@@ -1240,6 +1240,7 @@ EXPORT_SYMBOL_GPL(vfs_kern_mount);
 static struct mount *clone_mnt(struct mount *old, struct dentry *root,
 					int flag)
 {
+	struct super_block *sb = old->mnt.mnt_sb;
 	struct mount *mnt;
 	int err;
 
