d02fbd1446a44fb96f21fbad2392cae2d4255fba
Mauro Rossi <issor.oruam@gmail.com>
2026-02-28 11:20:23 +0100

    Revert "mm: make vm_area_desc utilise vma_flags_t only"
    This reverts commit 53f1d936445131cb5da2212c2b60884a25cb0330.

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -887,7 +887,10 @@ struct vm_area_desc {
 	/* Mutable fields. Populated with initial state. */
 	pgoff_t pgoff;
 	struct file *vm_file;
-	vma_flags_t vma_flags;
+	union {
+		vm_flags_t vm_flags;
+		vma_flags_t vma_flags;
+	};
 	pgprot_t page_prot;
 
 	/* Write-only fields. */
