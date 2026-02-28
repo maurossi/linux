e7d88c0e562b1b1f14e1a6b206182386c6710e4f
Mauro Rossi <issor.oruam@gmail.com>
2026-02-28 11:20:13 +0100

    Revert "tools/testing/vma: add VMA userland tests for VMA flag functions"
    This reverts commit f615cc92641a403d354c6ee68263074a86de49c7.

--- tools/testing/vma/vma_internal.h
+++ tools/testing/vma/vma_internal.h
@@ -36,11 +36,11 @@
  * ahead of all other headers.
  */
 #define __private
-/* NUM_MM_FLAG_BITS defined by test code. */
+#define NUM_MM_FLAG_BITS (64)
 typedef struct {
 	__private DECLARE_BITMAP(__mm_flags, NUM_MM_FLAG_BITS);
 } mm_flags_t;
-/* NUM_VMA_FLAG_BITS defined by test code. */
+#define NUM_VMA_FLAG_BITS BITS_PER_LONG
 typedef struct {
 	DECLARE_BITMAP(__vma_flags, NUM_VMA_FLAG_BITS);
 } __private vma_flags_t;
